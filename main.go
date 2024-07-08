24b5d4161133fb7a844df9b832801573c8048f80
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-08 23:42:19 +0500

    cleint HistoryOrder and DepthOrder structs added

--- main.go
+++ main.go
@@ -1,23 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"time"
 )
 
+var (
+	db *gorm.DB
+)
+
+func init() {
+	var err error
+	dsn := "postgresql://postgres:postgres@localhost/postgres?sslmode=disable" // Update with your database credentials
+	db, err = gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	// Auto migrate the User model
+	db.AutoMigrate(
+		&Client{},
+		&HistoryOrder{},
+		&DepthOrder{},
+	)
+}
+
 func main() {
-	fmt.Println("Server is listening on 8282 port")
-	http.HandleFunc("/GetOrderBook", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "About Page")
-	})
-	http.HandleFunc("/SaveOrderBook", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "SavedOrderBook")
-	})
-	http.HandleFunc("/GetOrderHistory", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "OrderHistory")
-	})
-	http.HandleFunc("/SaveOrder", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "SavedOrder")
-	})
-	http.ListenAndServe(":8282", nil)
+}
+
+type Client struct {
+	client_name   string `json:"client_name"`
+	exchange_name string `json:"exchange_name"`
+	label         string `json:"label"`
+	pair          string `json:"pair"`
+}
+
+type HistoryOrder struct {
+	client_name           string    `json:"client_name`
+	exchange_name         string    `json:"exchange_name"`
+	label                 string    `json:label`
+	pair                  string    `json:pair`
+	side                  string    `json:"side`
+	type_                 string    `json:"type"`
+	base_qty              float64   `json:"base_qty"`
+	price                 float64   `json:"price"`
+	algorithm_name_placed string    `json:"algorithm_name_placed"`
+	lowest_sell_prc       float64   `json:"lowest_sell_prc"`
+	highest_buy_prc       float64   `json:"highest_buy_prc" `
+	commission_quote_qty  float64   `json:"commission_quote_qty" `
+	time_placed           time.Time `json:"time_placed"`
+}
+
+type DepthOrder struct {
+	Price   float64 `json:"price"`
+	BaseQty float64 `json:"base_qty"`
 }
