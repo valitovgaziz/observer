ad2a969b504deb7a5b4ac7c3a3f30af01d1b4609
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-08 16:17:12 +0500

    added handlers

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Cool!")
+	fmt.Println("Server is listening on 8282 port")
+	http.HandleFunc("/GetOrderBook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About Page")
+	})
+	http.HandleFunc("/SaveOrderBook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "SavedOrderBook")
+	})
+	http.HandleFunc("/GetOrderHistory", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "OrderHistory")
+	})
+	http.HandleFunc("/SaveOrder", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "SavedOrder")
+	})
+	http.ListenAndServe(":8282", nil)
 }
