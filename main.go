ca3dacca6f729fd6cdcc5de020d6845c93e570e4
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-09 12:45:09 +0500

    rouating added into main

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -27,7 +29,6 @@ func init() {
 	)
 }
 
-
 type Client struct {
 	Client_name   string `json:"client_name"`
 	Exchange_name string `json:"exchange_name"`
@@ -41,31 +42,64 @@ type DepthOrder struct {
 }
 
 type HistoryOrder struct {
-	client_name           string    `json:"client_name"`
-	exchange_name         string    `json:"exchange_name"`
-	label                 string    `json:"label"`
-	pair                  string    `json:"pair"`
-	side                  string    `json:"side"`
-	type_                 string    `json:"type"`
-	base_qty              float64   `json:"base_qty"`
-	price                 float64   `json:"price"`
-	algorithm_name_placed string    `json:"algorithm_name_placed"`
-	lowest_sell_prc       float64   `json:"lowest_sell_prc"`
-	highest_buy_prc       float64   `json:"highest_buy_prc"`
-	commission_quote_qty  float64   `json:"commission_quote_qty"`
-	time_placed           time.Time `json:"time_placed"`
+	Client_name           string    `json:"client_name"`
+	Exchange_name         string    `json:"exchange_name"`
+	Label                 string    `json:"label"`
+	Pair                  string    `json:"pair"`
+	Side                  string    `json:"side"`
+	Type_                 string    `json:"type"`
+	Base_qty              float64   `json:"base_qty"`
+	Price                 float64   `json:"price"`
+	Algorithm_name_placed string    `json:"algorithm_name_placed"`
+	Lowest_sell_prc       float64   `json:"lowest_sell_prc"`
+	Highest_buy_prc       float64   `json:"highest_buy_prc"`
+	Commission_quote_qty  float64   `json:"commission_quote_qty"`
+	Time_placed           time.Time `json:"time_placed"`
 }
 
-func getClients(c *gin.Context) {
-	var clients []Client
-	db.Find(&clients)
-	c.JSON(200, gin.H{"data": clients})
+func SaveOrderBook(c *gin.Context) {
+	var order DepthOrder
+	c.BindJSON(&order)
+	eroor := db.Create(&order)
+	if eroor != nil {
+		c.JSON(400, gin.H{"error": eroor})
+		return
+	}
+	c.JSON(200, gin.H{"message": "Order book saved"})
+	return
+}
+
+func GetOrderBook(c *gin.Context) {
+	var orders []HistoryOrder
+	db.Find(&orders)
+	c.JSON(200, gin.H{"data": orders})
+}
+
+func SaveOrder(c *gin.Context) {
+	var order HistoryOrder
+	c.BindJSON(&order)
+	error := db.Create(&order)
+	if error != nil {
+		c.JSON(400, gin.H{"error": error})
+		return
+	}
+	c.JSON(200, gin.H{"message": "Order saved"})
+	return
+}
+
+func GetOrderHistory(c *gin.Context) {
+	var orders []HistoryOrder
+	db.Find(&orders)
+	c.JSON(200, gin.H{"data": orders})
 }
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/clients", getClients)
+	r.GET("/GetOrderBook", GetOrderBook)
+	r.POST("/SaveOrderBook", SaveOrderBook)
+	r.GET("/GetOrderHistory", GetOrderHistory)
+	r.POST("/GetOrderHistory", SaveOrder)
 
 	r.Run(":8282")
 	fmt.Println("Listening on port 8282")
